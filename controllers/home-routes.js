af93586c3edd03ca12c5fa8e2ba10f217b0413c8
Julio Rios <rios.julio.david@outlook.com>
2021-11-27 17:44:01 -0400

    Update Home Routes
    render home page with all categories added in database
    - to be selected as an option in the signup modal

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,23 @@
 const router = require('express').Router();
+const {Category} = require('../models')
 
 // Render home page
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        loggedIn: req.session.loggedIn
-    });
+
+    Category.findAll({
+        attributes: ["id", "category_name"]
+    })
+    .then(categoriesData => {
+        const category = categoriesData.map(category => category.get({ plain: true }));
+        console.log(category);
+        
+        res.render('homepage', {
+            loggedIn: req.session.loggedIn,
+            category: category
+        });
+    })
+
+
 });
 
 module.exports = router;
