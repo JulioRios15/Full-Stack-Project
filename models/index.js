41ab4f9abf0dbb5d0839fa6d0937ab6d50f0aef0
z0mbiebrad <Bradleyt.evans@gmail.com>
2021-11-29 18:42:41 -0500

    more work on post routes and table connections

--- models/index.js
+++ models/index.js
@@ -5,6 +5,19 @@ const Post = require("./Post");
 
 //TODO: 
 // Create Assosiations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+Category.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+User.hasMany(Category, {
+    foreignKey: 'user_id'
+});
 
 
 module.exports = { 
