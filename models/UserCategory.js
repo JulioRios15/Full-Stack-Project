55303740261d069d481dc26e13ef7a0959ee1b11
Julio Rios <rios.julio.david@outlook.com>
2021-11-29 22:19:48 -0400

    Add User Category Model

---
+++ models/UserCategory.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserCategory extends Model {}
+
+UserCategory.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user_category',
+  }
+);
+
+module.exports = UserCategory;
