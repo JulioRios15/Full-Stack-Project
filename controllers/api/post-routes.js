9efd7a232cb65aee9bd5dab4aba6ade833f80f86
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2021-11-30 22:56:53 -0400

    Update multer storage

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Category, UserCategory } = require('../../models');
+const {Op} = require('sequelize')
+const upload = require('../../middlewares/multer');
 
 // get posts
 router.get('/', (req, res) => {
@@ -7,7 +9,7 @@ router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
             'id', 
-            'post_url', 
+            'image_src', 
             'title', 
             'created_at'
         ],
@@ -15,6 +17,10 @@ router.get('/', (req, res) => {
             {
                 model: User,
                 attributes:['username']
+            },
+            {
+                model: Category,
+                attributes: ["category_name"]
             }
         ]
     })
@@ -24,5 +30,63 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+//Get All posts or get all post from user selected categories
+router.get('/category', async (req, res) => {
+    try {
+        const userId = req.session.user_id || 3;
+        const UserCategoriesData = await UserCategory.findAll({where: {
+            user_id: userId
+        }});
+
+        const categoriesIdsArr = UserCategoriesData.map((item) => item.category_id);
+        const allPosts = await Post.findAll({
+            include: [
+                {model: User},
+                {model: Category}
+            ]
+        });
+
+        const userCategoryPosts = await Post.findAll({
+            where: {
+                category_id: categoriesIdsArr
+            }, 
+            include: [
+                {model: User}, 
+                {model: Category}
+            ]
+        });
+
+        const posts = (categoriesId.length)? userCategoryPosts : allPosts;
+        return res.json(posts);
+        
+    } catch (error) {
+        return res.status(500).json({error})
+    }
+});
+
+router.post('/', upload.single('file'), async (req, res) => {
+    const {title, category_name} = req.body;
+    const fileName = req.file.filename;
+    const image_src = `images/${category_name}/${fileName}`;
+    const userId = req.session.user_id;
+
+    try {
+        const category = await Category.findOne({where: {category_name}});
+
+        const newPost = await Post.create({
+            title,
+            image_src,
+            user_id: userId,
+            category_id: category.id
+        });
+
+        return res.status(200).json(newPost);
+        
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+
+})
 
 module.exports = router;
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 //Setup Multer for file storage
 const fileStorageEngine = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, `./public/images/${req.query.category_name}`);
+        cb(null, `./public/images/${req.body.category_name}`);
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}--${file.originalname}`);
