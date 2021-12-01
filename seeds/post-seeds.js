4b4bce57215c3867d230eeb0125c00d1e6715f86
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2021-12-01 11:03:55 -0400

    Update Posts Seeds
    fix imgage src path to match directory name, image -> images

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,97 +3,97 @@ const { Post } = require('../models');
 const postData = [
     {
         title: "Wallie",
-        image_src: "image/Skateboarding/alex-perez-LlwHorAMpFo-unsplash.jpg",
+        image_src: "images/Skateboarding/alex-perez-LlwHorAMpFo-unsplash.jpg",
         user_id: 2,
         category_id: 1
     },
     {
         title: "50-50 Bowl",
-        image_src: "image/Skateboarding/ash-edmonds-oqyoL_G6ZHA-unsplash.jpg",
+        image_src: "images/Skateboarding/ash-edmonds-oqyoL_G6ZHA-unsplash.jpg",
         user_id: 2,
         category_id: 1
     },
     {
         title: "Beach Bowl",
-        image_src: "image/Skateboarding/bart-dunweg-ZICn6npvp6U-unsplash.jpg",
+        image_src: "images/Skateboarding/bart-dunweg-ZICn6npvp6U-unsplash.jpg",
         user_id: 2,
         category_id: 1
     },
     {
         title: "Daniel Lincoln Skate",
-        image_src: "image/Skateboarding/daniel-lincoln-OT87LknF3zc-unsplash.jpg",
+        image_src: "images/Skateboarding/daniel-lincoln-OT87LknF3zc-unsplash.jpg",
         user_id: 2,
         category_id: 1
     },
     {
         title: "Mini Ramp",
-        image_src: "image/Skateboarding/jan-kopriva-3yaPHboMrBg-unsplash.jpg",
+        image_src: "images/Skateboarding/jan-kopriva-3yaPHboMrBg-unsplash.jpg",
         user_id: 1,
         category_id: 1
     },
     {
         title: "Surfer",
-        image_src: "image/Surfing/asaf-r-7jSkK3DLmnM-unsplash.jpg",
+        image_src: "images/Surfing/asaf-r-7jSkK3DLmnM-unsplash.jpg",
         user_id: 1,
         category_id: 2
     },
     {
         title: "Body Surfer Barrel",
-        image_src: "image/Surfing/drew-farwell-1k1e9-cQ3D4-unsplash.jpg",
+        image_src: "images/Surfing/drew-farwell-1k1e9-cQ3D4-unsplash.jpg",
         user_id: 2,
         category_id: 2
     },
     {
         title: "Surfer Barrel",
-        image_src: "image/Surfing/jeremy-bishop-pikyGuAmwpM-unsplash.jpg",
+        image_src: "images/Surfing/jeremy-bishop-pikyGuAmwpM-unsplash.jpg",
         user_id: 1,
         category_id: 2
     },
     {
         title: "Air",
-        image_src: "image/Surfing/mathieu-chirico-sFSZuKI2CvY-unsplash.jpg",
+        image_src: "images/Surfing/mathieu-chirico-sFSZuKI2CvY-unsplash.jpg",
         user_id: 1,
         category_id: 2
     },
     {
         title: "Thomas Ashlock Big Wave Barrel",
-        image_src: "image/Surfing/thomas-ashlock-7G5dkthFyxA-unsplash.jpg",
+        image_src: "images/Surfing/thomas-ashlock-7G5dkthFyxA-unsplash.jpg",
         user_id: 1,
         category_id: 2
     },
     {
         title: "Andrew Popov Fire",
-        image_src: "image/BMX/alexander-popov-LbLS9mQspW4-unsplash.jpg",
+        image_src: "images/BMX/alexander-popov-LbLS9mQspW4-unsplash.jpg",
         user_id: 2,
         category_id: 3
     },
     {
         title: "Jan Kopriva",
-        image_src: "image/BMX/jan-kopriva-sQ1ZfaaXKvI-unsplash.jpg",
+        image_src: "images/BMX/jan-kopriva-sQ1ZfaaXKvI-unsplash.jpg",
         user_id: 2,
         category_id: 3
     },
     {
         title: "Andrey Shatalin",
-        image_src: "image/Motocross/andrey-shatalin-jwe7ASyC2mM-unsplash.jpg",
+        image_src: "images/Motocross/andrey-shatalin-jwe7ASyC2mM-unsplash.jpg",
         user_id: 2,
         category_id: 4
     },
     {
         title: "Giuseppe",
-        image_src: "image/Motocross/giuseppe-patriarchi-HZpWQdUCqVI-unsplash.jpg",
+        image_src: "images/Motocross/giuseppe-patriarchi-HZpWQdUCqVI-unsplash.jpg",
         user_id: 1,
         category_id: 4
     },
     {
         title: "Felipe Insane Air",
-        image_src: "image/Snowboarding/felipe-giacometti-sawa7hivsRs-unsplash.jpg",
+        image_src: "images/Snowboarding/felipe-giacometti-sawa7hivsRs-unsplash.jpg",
         user_id: 1,
         category_id: 5
     },
     {
         title: "Snow",
-        image_src: "image/Snowboarding/alesia-kazantceva-9-dHYri9BOE-unsplash.jpg",
+        image_src: "images/Snowboarding/alesia-kazantceva-9-dHYri9BOE-unsplash.jpg",
         user_id: 2,
         category_id: 5
     },
